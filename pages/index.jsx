c4333817abf0b83c71ee095eaa554ca6a962a8d2
JahazieHernandezHoyos <jahazielhernandezhoyoz@gmail.com>
2022-07-31 17:50:17 -0500

    added validation

---
+++ pages/index.jsx
@@ -0,0 +1,114 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+// Enunciado:
+
+// Haz una web simulador de cuota de hipoteca. Para ello, el usuario proporcionará: cantidad (€), número de cuotas (meses) y tipo de interés (%). La página devolverá el importe de la cuota mensual.
+
+// Opcional: puedes usar un slider en el número de cuotas y en el tipo de interés.
+
+// Consideraciones: Debemos poder probar el ejercicio, ya sea con jsfiddle, stackblitz o publicado en algún sitio.
+
+export default function Home() {
+  function calculateMonthlyPayment(amount, months, interest) {
+    //validar que los datos sean correctos
+    if (amount <= 0 || months <= 0 || interest <= 0) {
+      return "Introduzca un monto valido";
+    }
+    const monthlyInterest = interest / 100 / 12;
+    const monthlyPayment = amount * monthlyInterest / (1 - Math.pow(1 / 1 + monthlyInterest, -months));
+    return monthlyPayment;
+  }
+  return (
+    //div que abarque el 100% de la pantalla
+    <div className={styles.container}>
+      <Head>
+        <title>Hipoteca</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Hipoteca</h1>
+        <p className={styles.description}>
+          Proporciona cantidad, número de cuotas y tipo de interés.
+        </p>
+        <div className={styles.grid}>
+          <div className={styles.card}>
+            <div className={styles.cardContent}>
+              <div className={styles.cardTitle}>
+                <h3>Cantidad</h3>
+              </div>
+              <div className={styles.cardBody}>
+                <div className={styles.inputContainer}>
+                  <input
+                    type="text"
+                    name="cantidad"
+                    id="cantidad"
+                    className={styles.input}
+                    placeholder="0.00€"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.cardContent}>
+              <div className={styles.cardTitle}>
+                <h3>Número de cuotas</h3>
+              </div>
+              <div className={styles.cardBody}>
+                <div className={styles.inputContainer}>
+                  <input
+                    type="text"
+                    name="cuotas"
+                    id="cuotas"
+                    className={styles.input}
+                    placeholder="0"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.card}>
+            <div className={styles.cardContent}>
+              <div className={styles.cardTitle}>
+                <h3>Tipo de interés</h3>
+              </div>
+              <div className={styles.cardBody}>
+                <div className={styles.inputContainer}>
+                  <input
+                    type="text"
+                    name="interes"
+                    id="interes"
+                    className={styles.input}
+                    placeholder="0.00%"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.buttonContainer}>
+        {/* boton de calcular con animaciony activando funcion para calcular hipoteca */}
+          <button
+            className={styles.button}
+            onClick={() => {
+              const amount = document.getElementById("cantidad").value;
+              const months = document.getElementById("cuotas").value;
+              const interest = document.getElementById("interes").value;
+              const monthlyPayment = calculateMonthlyPayment(
+                amount,
+                months,
+                interest
+              );
+              alert(`El importe mensual es de ${monthlyPayment}€`);
+            }
+            }
+          >
+            Calcular
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+}
